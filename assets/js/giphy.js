08402ecfd92bc3c4fa1caa28a392196db6bc7de5
Randy Reitz <rsreitz@outlook.com>
2018-03-11 11:26:57 -0400

    Allows user to add buttons.

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -3,7 +3,37 @@ var gifTags = ["cat", "dog", "platypus", "unicorn", "The Rock"];
 $(document).ready(function() {
     loadButtons(gifTags);
 
-    $("button").on("click", function() {
+    //  On Click event associated with the add-to-do function
+    $("#add-to-tags").on("click", function(event) {
+        event.preventDefault();
+
+        addButtonTag($("#add-tag").val().trim());
+        // // Clear the textbox when done
+        $("#add-tag").val("");
+    });
+
+});
+
+function addButtonTag(tag) {
+    gifTags.push(tag);
+    loadButtons(gifTags);
+}
+
+function makeButton(tag) {
+    var b = $("<button>").attr("gif-tag", tag).addClass("tag-btn").text(tag);
+    $("#buttons").append(b).append("\n");
+}
+
+function loadButtons(tagList) {
+    $("#buttons").empty();
+    tagList.forEach(element => {
+        makeButton(element)
+    });
+    addTagButtonClicks();
+}
+
+function addTagButtonClicks() {
+    $(".tag-btn").on("click", function() {
         var gifTag = $(this).attr("gif-tag");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             gifTag + "&api_key=Lag4SW0Rvz64rsiP0rAttQGD3C3zzl4U&limit=10";
@@ -32,16 +62,5 @@ $(document).ready(function() {
                 }
             });
     });
-});
 
-
-function makeButton(tag) {
-    var b = $("<button>").attr("gif-tag", tag).text(tag);
-    $("#buttons").append(b).append("\n");
-}
-
-function loadButtons(tagList) {
-    tagList.forEach(element => {
-        makeButton(element)
-    });
 }
